c304bdf96445543ddd5ab63df9727e051008e957
Etcherla Sai Manoj <esmanoj.95@gmail.com>
2022-01-27 11:39:33 +0530

    Codechef Starters 23

---
+++ codechef/codechef_starters_23/averageOfThree.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int testcases;
+	cin >> testcases;
+	while(testcases--){
+	    int n;
+	    cin >> n;
+	    cout << n - 1 << " " << n << " " << n + 1 << "\n";
+	}
+	return 0;
+}
